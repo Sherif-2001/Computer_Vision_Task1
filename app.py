aa287bca7c031f06394e345af9f1c47026fe4b11
sherif <sherifahmedelsayed2@gmail.com>
2023-03-08 19:49:09 +0200

    add routes and save images to assets

--- app.py
+++ app.py
@@ -1,15 +1,82 @@
 from flask import Flask, render_template, request
-skills_app = Flask(__name__, static_url_path='')
+import cv2
+from PIL import Image
 
+app = Flask(__name__)
 
-@skills_app.route("/")
+def saveImage(image, index):
+    imageShow = Image.open(image)
+    imageShow.save(f"static/assets/image{index}.png")
+
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@skills_app.route("/test",methods=['POST'])
-def test():
-    image1=request.files['image1'].read()
-    image2=request.files['image2'].read()
-    operation= request.form['operation']
-    return "diaa"
+# @app.route("/test",methods=['POST'])
+# def test():
+
+#     image1 = request.files['image1']
+#     saveImage(image1, 1)
+
+#     image2 = request.files['image2']
+#     saveImage(image2, 2)
+
+#     operation = request.form['operation']
+#     return "diaa"
+
+@app.route("/tab1")
+def tab1():
+    image1 = request.files['image1']
+    saveImage(image1, 1)
+
+    
+
+    return "tab1"
+
+@app.route("/tab2")
+def tab2():
+    image1 = request.files['image1']
+    saveImage(image1, 1)
+    
+    return "tab2"
+
+@app.route("/tab3")
+def tab3():
+    image1 = request.files['image1']
+    image2 = request.files['image2']
+    
+    saveImage(image1, 1)
+    saveImage(image2, 2)
+
+    return "tab3"
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
+# @app.route("/image/<id>", methods = ["GET", "POST"])
+# def uploadImage(id):
+#     if(request.method == "POST"):
+#         file = request.data
+#         img = ImageSave.open(io.BytesIO(file))
+#         if (id == "1"):
+#         elif(id == "2"):
+#     return "image processed"
+
+
+# $.ajax({
+#   type: "POST",
+#   url: "/process_qtc",
+#   data: JSON.stringify(server_data),
+#   contentType: "application/json",
+#   dataType: 'json' 
+# });
+
+# def process_qt_calculation():
+#   if request.method == "POST":
+#     qtc_data = request.get_json()
+#     print(qtc_data)
+#  results = {'processed': 'true'}
+#  return jsonify(results)
 
